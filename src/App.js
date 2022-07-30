f7f019c1efae47ecf6de55cdc2acbd6157e785cc
Leor Porat <lporat874@student.fuhsd.org>
2022-07-29 23:46:11 -0700

    re-styled navbar, reorganized files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './Navbar';
 
 /*
 function App() {
@@ -23,41 +24,10 @@ function App() {
 }
 */
 
-function Header() {
+function App() {
   return (
-    <html>
-      <header>
-        
-        <nav>
-          <div className="headerTab" id="poop">
-            <h1><strong>Leor<br></br>Porat</strong></h1>
-          </div>
-          <div className="headerTab">
-            <p>About</p>
-          </div>
-          <div className="headerTab">
-            <p>Research</p>
-          </div>
-          <div className="headerTab">
-            <p>Education</p>
-          </div>
-          <div className="headerTab">
-            <p>Personal</p>
-          </div>
-          <div className="headerTab">
-            <p>Projects</p>
-          </div>
-        </nav>
-      </header>
-      <body>
-        <h1>Hey, I'm Leor. I'm: </h1>
-        <ul className="aboutList">
-          <li>An incoming freshman at the University of Minnesota</li>
-        </ul>
-        
-      </body>
-    </html>
+    <Navbar />
   );
 }
 
-export default Header;
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,54 @@
+function Navbar() {
+    return (
+        /*
+        <html>
+          <header>
+            
+            <nav>
+              <div className="headerTab" id="poop">
+                <h1><strong>Leor<br></br>Porat</strong></h1>
+              </div>
+              <div className="headerTab">
+                <p>About</p>
+              </div>
+              <div className="headerTab">
+                <p>Research</p>
+              </div>
+              <div className="headerTab">
+                <p>Education</p>
+              </div>
+              <div className="headerTab">
+                <p>Personal</p>
+              </div>
+              <div className="headerTab">
+                <p>Projects</p>
+              </div>
+            </nav>
+          </header>
+          <body>
+            <h1>Hey, I'm Leor. I'm: </h1>
+            <ul className="aboutList">
+              <li>An incoming freshman at the University of Minnesota</li>
+            </ul>
+            
+          </body>
+        </html>
+        */
+       <nav>
+        <a href="/" className="site-title">Leor Porat</a>
+        <ul>
+            <li>
+                <a href="/education">Education</a>
+            </li>
+            <li>
+                <a href="/projects">Projects</a>
+            </li>
+            <li>
+                <a href="/personal">Personal</a>
+            </li>
+        </ul>
+       </nav>
+    )
+}
+
+export default Navbar;
