b9009b53c8bd7782af9a097ee8bd2ef1f480c6ef
Leor Porat <lporat874@student.fuhsd.org>
2022-08-01 00:03:50 -0700

    added tab title & photo (salute w)

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,40 +1,10 @@
 function Navbar() {
     return (
-        /*
-        <html>
-          <header>
-            
-            <nav>
-              <div className="headerTab" id="poop">
-                <h1><strong>Leor<br></br>Porat</strong></h1>
-              </div>
-              <div className="headerTab">
-                <p>About</p>
-              </div>
-              <div className="headerTab">
-                <p>Research</p>
-              </div>
-              <div className="headerTab">
-                <p>Education</p>
-              </div>
-              <div className="headerTab">
-                <p>Personal</p>
-              </div>
-              <div className="headerTab">
-                <p>Projects</p>
-              </div>
-            </nav>
-          </header>
-          <body>
-            <h1>Hey, I'm Leor. I'm: </h1>
-            <ul className="aboutList">
-              <li>An incoming freshman at the University of Minnesota</li>
-            </ul>
-            
-          </body>
-        </html>
-        */
         <div className='nav-parent'>
+            <head>
+                <title>Leor Porat</title>
+                <link rel="shortcut icon" href="./pictures/salute.pn"/>
+            </head>
             <nav>
                 <a href="/" className="site-title">Leor Porat</a>
                 <ul>
