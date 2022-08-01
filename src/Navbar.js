af7093a2ad37a6e8833d051ad335c35f41d09075
Leor Porat <lporat874@student.fuhsd.org>
2022-08-01 00:57:21 -0700

    added functional salute photo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,16 @@
+import salute from './pictures/salute.png'
+
 function Navbar() {
     return (
         <div className='nav-parent'>
             <head>
                 <title>Leor Porat</title>
-                <link rel="shortcut icon" href="./pictures/salute.pn"/>
+                <link rel="shortcut icon" href={salute}/>
             </head>
             <nav>
-                <a href="/" className="site-title">Leor Porat</a>
+                <div>
+                    <a href="/" className="site-title">Leor Porat</a>
+                </div>
                 <ul>
                     <SetActiveRoute href="/education">Education</SetActiveRoute>
                     <SetActiveRoute href="/projects">Projects</SetActiveRoute>
