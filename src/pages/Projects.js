268913e82e0f1568a3f0aeec80a616031f7439bf
Leor Porat <lporat874@student.fuhsd.org>
2022-08-03 18:20:41 -0700

    began projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,21 @@
 function Projects() {
     return (
-        <h1>Projects</h1>
+        <body>
+            <div className='projects-title'>
+                <div className='line-left'><p>Hey</p></div>
+                <h1>Projects</h1>
+                <div className='line-right'></div>
+            </div>
+            <div className='projects-parent'>
+                <div className='projects-body'>
+                    <div className='projects-personal-website'>
+                        <h3>This website <div className='projects-date'>(July 2022)</div></h3>
+                        <p>Built with HTML, CSS, React.js</p>
+                    </div>
+                </div>
+                    
+            </div>
+        </body>
     );
 }
 
